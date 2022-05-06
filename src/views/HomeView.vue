30b24fc01eae6955449a2853cef0d98c22d0a58f
danachen <danac.tw@gmail.com>
2022-05-06 10:55:50 +0800

    init settings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="map" class="map" ref="mapContainer"></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowRef, onMounted, onUnmounted, markRaw } from 'vue';
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "HomeView",
+  setup () {
+    const mapContainer = shallowRef(null);
+    const map = shallowRef(null);
+
+    onMounted(() => {
+      map.value = markRaw(new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          })
+        ],
+        target: 'map',
+        view: new View({
+          center: [0, 0],
+          zoom: 2,
+          constrainRotation: 16,
+        }),
+      }));
+
+    }),
+    onUnmounted(() => {
+      map.value?.remove();
+    })
+
+    return {
+      map, mapContainer
+    };
   }
-}
+};
 </script>
+
+<style>
+.map {
+  position: absolute;
+  width: 100%;
+  height: 80%;
+}
+</style>
