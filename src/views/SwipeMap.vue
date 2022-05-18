8979345798b99f9edd8ea66e67302de817e05b49
danachen <danac.tw@gmail.com>
2022-05-18 15:40:43 +0800

    Swipe Map: add layer spy

--- src/views/SwipeMap.vue
+++ src/views/SwipeMap.vue
@@ -1,4 +1,5 @@
 <template>
+  <span>Use the ↑ up and ↓ down arrow keys to adjust the spyglass size.</span>
   <input id="topSwipe" type="range" style="width: 100%" v-model="topSwipe">
   <div id="map" class="map" ref="mapContainer"></div>
   <input id="bottomSwipe" type="range" style="width: 100%" v-model="bottomSwipe">
@@ -9,7 +10,7 @@ import { shallowRef, onMounted, markRaw, ref, watch } from 'vue';
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import {OSM} from 'ol/source';
+import { OSM, BingMaps } from 'ol/source';
 import { Tile as TileLayer } from 'ol/layer';
 import { getRenderPixel } from 'ol/render';
 
@@ -19,6 +20,15 @@ export default {
     const map = shallowRef(null);
     const topSwipe = ref(50)
     const bottomSwipe = ref(50)
+    const mousePosition = ref(null)
+    const spyRadius = ref(75)
+
+    const BingMap = new TileLayer({
+      source: new BingMaps({
+        key: "Ak6hRbflYvuARcql-hA_RTu20jkC6UQGDi4PLkJ1vOHrD30DD-1et3-N3DSohoPA",
+        imagerySet: "AerialWithLabelsOnDemand"
+      })
+    });
 
     const OSMHumanitarian = new TileLayer({
       source: new OSM({
@@ -52,13 +62,41 @@ export default {
       ctx.restore();
     });
 
+    BingMap.on('prerender', function (event) {
+      const ctx = event.context;
+      ctx.save();
+      ctx.beginPath();
+      if (mousePosition.value) {
+        // only show a circle around the mouse
+        const pixel = getRenderPixel(event, mousePosition.value);
+        const offset = getRenderPixel(event, [
+          mousePosition.value[0] + spyRadius.value,
+          mousePosition.value[1],
+        ]);
+        const canvasRadius = Math.sqrt(
+          Math.pow(offset[0] - pixel[0], 2) + Math.pow(offset[1] - pixel[1], 2)
+        );
+        ctx.arc(pixel[0], pixel[1], canvasRadius, 0, 2 * Math.PI);
+        ctx.lineWidth = (5 * canvasRadius) / spyRadius.value;
+        ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+        ctx.stroke();
+      }
+      ctx.clip();
+    });
+
+    BingMap.on('postrender', function (event) {
+      const ctx = event.context;
+      ctx.restore();
+    });
+
     onMounted(() => {
       map.value = markRaw(new Map({
         layers: [
           new TileLayer({
             source: new OSM(),
           }),
-          OSMHumanitarian
+          OSMHumanitarian,
+          BingMap,
         ],
         target: 'map',
         view: new View({
@@ -66,6 +104,14 @@ export default {
           zoom: 2,
         }),
       }))
+
+      map.value.on('pointermove', (e) => {
+        const XBorder = map.value.getSize()[0]-75
+        const YBorder = map.value.getSize()[1]-75
+        if (e.pixel[0] < 75 || e.pixel[0] > XBorder || e.pixel[1] < 75 || e.pixel[1] > YBorder) mousePosition.value = null
+        else mousePosition.value = e.pixel
+        map.value.render();
+      })
 
       const listener = function () {
         map.value.render();
